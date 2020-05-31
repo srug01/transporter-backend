4012e3bb8c9f3fc2095ff381c29a964efeeb804c
utsav-hub <utsavprht06@gmail.com>
2020-05-31 19:50:59 +0530

    mixins

---
+++ src/mixins/modifiedby-property-mixin.ts
@@ -0,0 +1,22 @@
+/* import {Model, property} from '@loopback/repository';
+
+/**
+ * A mixin factory to add `category` property
+ *
+ * @param superClass - Base Class
+ * @typeParam T - Model class
+ */
+/*
+export function AddCategoryPropertyMixin<T extends MixinTarget<Model>>(
+  superClass: T,
+) {
+  class MixedModel extends superClass {
+    @property({
+      type: 'string',
+      required: true,
+    })
+    category: string;
+  }
+  return MixedModel;
+}
+ */
