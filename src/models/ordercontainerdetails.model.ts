78a102e8701adc45be15b10a6d6ba9e6082779ef
Bhushan Gadekar <b.gadekar@crif.com>
2020-06-20 15:00:52 +0530

    added code

--- src/models/ordercontainerdetails.model.ts
+++ src/models/ordercontainerdetails.model.ts
@@ -1,20 +1,23 @@
-import {Entity, hasMany, model, property} from '@loopback/repository';
+import {belongsTo, Entity, hasMany, model, property} from '@loopback/repository';
+import {Orders} from './orders.model';
 import {Ordertruckdetails} from './ordertruckdetails.model';
 
 @model()
 export class Ordercontainerdetails extends Entity {
   @property({
     type: 'number',
-    id: true,
-    generated: true,
+    id: true
   })
   order_container_syscode?: number;
 
-  @property({
-    type: 'number',
-    required: true,
-  })
-  order_syscode: number;
+  // @property({
+  //   type: 'number',
+  //   required: true,
+  // })
+  // order_syscode: number;
+
+  @belongsTo(() => Orders)
+  order_syscode_key: number;
 
   @property({
     type: 'number',
@@ -61,6 +64,9 @@ export class Ordercontainerdetails extends Entity {
   })
   modify_on?: string;
 
+  @property({
+    type: 'any'
+  })
   @hasMany(() => Ordertruckdetails, {keyTo: 'order_container_syscode'})
   order_container_numbers: Ordertruckdetails[];
 
