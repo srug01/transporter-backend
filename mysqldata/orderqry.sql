765f114d7494456435154c98fa561c34a4a0bbb6
utsav-hub <utsavprht06@gmail.com>
2020-08-20 00:17:23 +0530

    sql

--- mysqldata/orderqry.sql
+++ mysqldata/orderqry.sql
@@ -4,18 +4,31 @@ Select * From transporter.bid;
 Select * from transporter.user;
 Select * from transporter.trip;
 
+Select * from transporter.bidusermapping; 
+Select * from transporter.container;
+select * from transporter.truck;
+Select * from transporter.driver;
+Select * from transporter.yardmaster;
+select * from transporter.portmaster;
+
 Call procSchedulerConfirmBid();
 Call procGetOrderDetails(null);
+Call getDashboardForAdmin(1);
+Call getDashboardForCFS(20);
+Call getDashboardForTransporter(11);
+Call getDashboardForDriver(23);
 Call GetAllTripsbyUserId(11);
 Call GetAllSubOrdersbyUserId(14);
+Call GetTripDetailsbyTripId(1);
 
-Select transporter.bidusermapping.* from transporter.bidusermapping; 
 
+
+-- UPDATE `transporter`.`bidschedulemaster` SET `TotalBidHour` = '6' WHERE (`BidScheduleId` = '4');
 Update transporter.suborder set BidscheduleId = null, CutOffTime = null where orderId <> 1;
 
 Select * from transporter.statusMaster; -- 1 bid 2 bidsuer 3 order 4 suborder 5 trip
 Select * from transporter.StatusDetails;
-Select * from transporter.user;
+Delete from transporter.user where userId <> 1;
 Select * from transporter.container;
 Select * from transporter.truck;
 Select * from transporter.timeslotmaster;
